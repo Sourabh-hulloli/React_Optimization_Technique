9c72525bbfd933db4f18a8df08855b8c1a3ca1d5
sourabh hulloli <sourabh.hulloli@gmail.com>
2023-09-01 17:24:25 +0530

    How Virtual DOM works in React example code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
 import "./App.css";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log("App Running");
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParagraph) => {
+      return !prevShowParagraph;
+    });
+  };
+
   return (
     <div className="app">
       <h1> Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
---
+++ src/components/Demo/MyParagraph.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const MyParagraph = (props) => {
+  console.log("My Paragraph Running");
+  return <p>{props.children}</p>;
+};
+
+export default MyParagraph;
