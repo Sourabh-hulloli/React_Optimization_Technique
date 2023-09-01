0048cd01317ff06967625d2fdfcda48fc1d6bc29
sourabh hulloli <sourabh.hulloli@gmail.com>
2023-09-01 18:11:51 +0530

    Using useCallback hook to optimize

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import Button from "./components/UI/Button/Button";
 import DemoOutput from "./components/Demo/DemoOutput";
 import "./App.css";
@@ -8,16 +8,16 @@ function App() {
 
   console.log("App Running");
 
-  const toggleParagraphHandler = () => {
+  const toggleParagraphHandler = useCallback(() => {
     setShowParagraph((prevShowParagraph) => {
       return !prevShowParagraph;
     });
-  };
+  }, []);
 
   return (
     <div className="app">
       <h1> Hi there!</h1>
-      <DemoOutput show={showParagraph} />
+      <DemoOutput show={false} />
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
